ab90b6094f95f367ce40563c9043da0541849b79
Daria Saburova <saburova@azoft.com>
2017-06-10 23:27:33 +0700

    added fucntion for converting size files

--- source/components/Folders/File/index.js
+++ source/components/Folders/File/index.js
@@ -24,15 +24,27 @@ export const styles = StyleSheet.create({
   }
 });
 
+const convertSize = (size) => {
+  if (Math.floor(size/(Math.pow(2, 10))) == 0) {
+    return {value: size, name: 'Байт'};
+  } else if (Math.floor(size/(Math.pow(2, 20))) == 0) {
+    return {value: size/(Math.pow(2, 10)), name: 'КБ'};
+  } else if (Math.floor(size/(Math.pow(2, 30))) == 0) {
+    return {value: size/(Math.pow(2, 20)), name: 'MБ'};
+  } else {
+    return {value: size/(Math.pow(2, 30)), name: 'ГБ'};
+  }
+}
+
 export default class File extends Component {
   render() {
     const { data } = this.props;
-    let size = data.size/(Math.pow(2, 20)); //конвертация размера в мегабайты (с учетом того, что приходящее значение указано в битах)
+    let size = convertSize(data.size); //конвертация размера в мегабайты (с учетом того, что приходящее значение указано в битах)
     return (
       <div className={ css(styles.fileElement) }>
         <InsertDriveFile className={ css(styles.icon) } color={ '#6f6b6c' } />
         <b className={ css(styles.b) }>{ data.name }</b>
-        <small className={ css(styles.small) }>{ size.toFixed(2) } МБ</small>
+        <small className={ css(styles.small) }>{ `${size.value.toFixed(2)} ${size.name}` }</small>
       </div>
     )
   }
